2d298ede3b6af03dea2dc1472a6c80e2f9e2523a
STICKHALL\yasse <yasseryasg@gmail.com>
2023-03-16 18:28:12 -0400

    MOD

---
+++ src/models/server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const cors = require('cors');
+const { createServer } = require('http');
+
+const { dbConnection } = require('../database/config.js');
+//const { socketController } = require('../sockets/controller');
+
+class Server {
+
+    constructor() {
+        this.app  = express();
+        this.port = process.env.PORT;
+        this.server = createServer( this.app );
+        this.io     = require('socket.io')(this.server)
+
+        this.paths = {
+            auth:       '/api/auth',
+            usuarios:   '/api/usuarios',
+            
+        }
+
+
+        // Conectar a base de datos
+        this.conectarDB();
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de mi aplicación
+        this.routes();
+
+        // Sockets
+        //this.sockets();
+    }
+
+    async conectarDB() {
+        await dbConnection();
+    }
+
+
+    middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura y parseo del body
+        this.app.use( express.json() );
+
+        // Directorio Público
+        this.app.use( express.static('public') );
+
+        // Fileupload - Carga de archivos
+        this.app.use( fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/src/',
+            createParentPath: true
+        }));
+        
+    }
+
+    routes() {
+        
+        this.app.use( this.paths.auth, require('../routes/auth.js'));       
+        this.app.use( this.paths.usuarios, require('../routes/usuarios'));     
+        
+    }
+
+    /*
+    sockets() {
+        this.io.on('connection', ( socket ) => socketController(socket, this.io ) )
+    }
+    */
+
+    listen() {
+        this.server.listen( this.port, () => {
+            console.log('Server Listened on port', this.port );
+        });
+    }
+
+}
+
+
+
+
+module.exports = Server;
